baefedab558e32f9a2853eaeb6e544c350728609
nayan <mdmizanur940@gmail.com>
2023-04-04 14:16:23 +0600

    new customer entry on live and recognition

--- Face-Recognition-MIS/faceclass.py
+++ Face-Recognition-MIS/faceclass.py
@@ -10,6 +10,7 @@ from Retinaface_gpu.Retina_gpu import RetinaGPU
 from Arcface_custom import ArcFaceRecog
 from utils.face_utils import *
 import time
+from csv import writer
 
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
@@ -21,11 +22,11 @@ class FaceClass:
         self.face_rec = ArcFaceRecog.ArcFace(arcface_model_path)
         self.threshold = threshold
         print('Recognition Threshold : ', threshold)
-        face_db = pd.read_csv('MIS_Face_DB.csv')
-        self.known_embeddings = face_db.iloc[:, 2:]
-        known_labels = face_db.iloc[:, 0]
+        self.face_db = pd.read_csv('MIS_Face_DB.csv')
+        self.known_embeddings = self.face_db.iloc[:, 2:]
+        known_labels = self.face_db.iloc[:, 0]
         self.known_labels = np.array(known_labels)
-        self.names = face_db.iloc[:, 1].unique()
+        self.names = self.face_db.iloc[:, 1]
 
         if torch.cuda.is_available():
             self.retina = RetinaGPU(0)  # (-1) for CPU, (0) for GPU
@@ -42,81 +43,6 @@ class FaceClass:
         else:
             return False
 
-    def face_recognition(self, image, scale):
-
-        t1 = time.time()
-        faces, face_region = self.retina.extract_faces(image, scale, alignment=True)
-        face_region = np.array(face_region)
-        org_img = image.copy()
-
-        tar_face = None
-        tar_region = [0,0,0,0]
-        name = ""
-        resize_to = 1280
-        img_resized = cv2.resize(org_img, (resize_to, resize_to))
-        image_out = img_resized.copy()
-
-        face_region_resized = []
-        for region in face_region:
-            resized_region = [region[0]*(resize_to/org_img.shape[1]), region[1]*(resize_to/org_img.shape[0]), region[2]*(resize_to/org_img.shape[1]), region[3]*(resize_to/org_img.shape[0])]
-            face_region_resized.append(resized_region)
-
-            print(region, resized_region, org_img.shape)
-        face_region_resized = np.array(face_region_resized)
-
-
-
-        ref = [400, 240 , 1000, 900]
-        #cv2.rectangle(img_resized, (400,240), (1000,900), (255,0,0), 5)
-        cv2.rectangle(img_resized, (840, 280), (1050, 630), (255, 0, 0), 5)
-        for face, region in zip(faces, face_region_resized):
-            #print(region, ref)
-            if self.IsInside(ref, region):
-                print('yes here')
-                tar_face = face
-                tar_region = region
-            else:
-                pass
-
-
-        if tar_face is None:
-            return img_resized, None, None
-        else:
-            try:
-                face = cv2.resize(tar_face, (112, 112))
-                embd = self.face_rec.calc_emb(face)
-                embd = np.array(embd)
-                probabilities = np.dot(self.known_embeddings, embd.T)
-                index = np.argmax(probabilities)
-                score = np.max(probabilities)
-
-                if score >= self.threshold:
-                    name_id = int(self.known_labels[index])
-                    name = self.names[name_id]
-                else:
-                    name = 'unknown'
-
-                current_time = datetime.now().strftime("%H:%M:%S")
-                # if name != 'unknown':
-                #     print('    Name :    {}     Score :    {}    Time: {}'.format(name, round(score, 3), current_time))
-            except:
-                print('Face Loading error, shape:', face.shape)
-
-            detected_name = np.array(name)
-
-
-
-            # print(face_region, face_region_resized, img_resized.shape)
-
-            image_out = draw_boxes(img_resized, face_region_resized, detected_name, scale)
-
-            t2 = time.time()
-            fps = round(1 / (t2 - t1), 1)
-            cv2.rectangle(image_out, (40, 0), (300, 70), (0, 255, 0), -1)
-            cv2.putText(image_out, f'FPS : {fps}', (50, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 128, 0),
-                        thickness=2, lineType=1)
-
-            return image_out
 
     def face_recognition_v2(self, image, scale, memory, same_unkwn_detected ):
 
@@ -125,7 +51,7 @@ class FaceClass:
         face_region = np.array(face_region)
         org_img = image.copy()
         tar_face = None
-
+        area = 0
 
 
         ref = [250, 75, 450, 350]
@@ -134,8 +60,14 @@ class FaceClass:
             print(region)
             if self.IsInside(ref, region):
                 print('yes here')
-                tar_face = face
-                tar_region = region
+
+                new_area = abs((region[3]-region[1])*(region[2]-region[0]))
+                if new_area > area:
+                    tar_face = face
+                    tar_region = region
+                    area = new_area
+                else:
+                    pass
             else:
                 pass
 
@@ -169,11 +101,30 @@ class FaceClass:
                     else:
                         same_unkwn_detected.clear()
 
-            if len(same_unkwn_detected) > 10:
+            if len(same_unkwn_detected) > 15:
                 print('New Person Detected')
                 same_unkwn_detected.clear()
-                for emb in memory[-10:]:
-                    print(emb)
+                id = len(self.face_db.index)
+                name = str(id+111)+'jja'
+                for emb in memory[-12:]:
+
+                    entry = [id, name]
+                    for i in emb:
+                        entry.append(i)
+                    print('Wriitng to DB',len(self.face_db.index))
+                    with open(
+                            '/home/nayan/PycharmProjects/CustomerRecognitionFunctionalModule/Face-Recognition-MIS/MIS_Face_DB.csv',
+                            'a') as f_object:
+                        writer_object = writer(f_object)
+                        writer_object.writerow(entry)
+                        f_object.close()
+                print('successfully added the new person in the DB')
+                print('Updating the application according to DB')
+                self.face_db = pd.read_csv('MIS_Face_DB.csv')
+                self.known_embeddings = self.face_db.iloc[:, 2:]
+                known_labels = self.face_db.iloc[:, 0]
+                self.known_labels = np.array(known_labels)
+                self.names = self.face_db.iloc[:, 1]
 
 
             current_time = datetime.now().strftime("%H:%M:%S")
@@ -181,7 +132,7 @@ class FaceClass:
             #     print('Face Loading error, shape:', face.shape)
 
             detected_name = name
-            print(same_unkwn_detected)
+            print(same_unkwn_detected, 'name', detected_name)
 
             # print(face_region, face_region_resized, img_resized.shape)
 
@@ -193,58 +144,3 @@ class FaceClass:
             cv2.putText(image_out, f'FPS : {fps}', (50, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 128, 0),
                         thickness=2, lineType=1)
             return org_img
-
-
-
-
-
-
-
-
-
-
-        # t1 = time.time()
-        # faces, face_region = self.retina.extract_faces(image, scale, alignment=True)
-        # face_region = np.array(face_region)
-        # org_img = image.copy()
-        # detected_names = []
-        # for face in faces:
-        #     try:
-        #         face = cv2.resize(face, (112, 112))
-        #         embd = self.face_rec.calc_emb(face)
-        #         embd = np.array(embd)
-        #         probabilities = np.dot(self.known_embeddings, embd.T)
-        #         index = np.argmax(probabilities)
-        #         score = np.max(probabilities)
-        #
-        #         if score >= self.threshold:
-        #             name_id = int(self.known_labels[index])
-        #             name = self.names[name_id]
-        #         else:
-        #             name = 'unknown'
-        #         detected_names.append(name)
-        #         current_time = datetime.now().strftime("%H:%M:%S")
-        #         # if name != 'unknown':
-        #         #     print('    Name :    {}     Score :    {}    Time: {}'.format(name, round(score, 3), current_time))
-        #     except:
-        #         print('Face Loading error, shape:', face.shape)
-        #
-        # detected_names = np.array(detected_names)
-        # resize_to = 1280
-        # img_resized = cv2.resize(org_img, (resize_to, resize_to))
-        # image_out = img_resized.copy()
-        # face_region_resized = []
-        # for region in face_region:
-        #     resized_region = [region[0]*(resize_to/org_img.shape[1]), region[1]*(resize_to/org_img.shape[0]), region[2]*(resize_to/org_img.shape[1]), region[3]*(resize_to/org_img.shape[0])]
-        #     face_region_resized.append(resized_region)
-        # face_region_resized = np.array(face_region_resized)
-        #
-        # for i in range(len(face_region)):
-        #     image_out = draw_boxes(img_resized, face_region_resized[i], detected_names[i], scale)
-        #
-        # t2 = time.time()
-        # fps = round(1 / (t2 - t1), 1)
-        # cv2.rectangle(image_out, (40, 0), (300, 70), (0, 0, 0), -1)
-        # cv2.putText(image_out, f'FPS : {fps}', (50, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 128, 0), thickness=2, lineType=1)
-        # return image_out, detected_names
-
